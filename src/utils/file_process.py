7923be503a8e056f3d0c4f5cbf9f4c71b2d6d58f
lavaqq <mlava@tuta.io>
2023-03-22 00:30:33 +0100

    feat(utils): rename fn

--- src/utils/file_process.py
+++ src/utils/file_process.py
@@ -1,2 +1,12 @@
-def test():
-    print("file")
+import os
+
+def rename(directory_path):
+    count = 0
+    for filename in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, filename)
+        if os.path.isfile(file_path) and any(file_path.lower().endswith(ext) for ext in ['.jpg', '.jpeg', '.png']):
+            new_filename = str(count) + '.png'
+            os.rename(file_path, os.path.join(directory_path, new_filename))
+            count += 1
+        else:
+            continue
