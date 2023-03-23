6f136137379c734d201168ff034ab9f1342c2e10
lavaqq <mlava@tuta.io>
2023-03-23 18:59:54 +0100

    refactor(utils): replace conversion fn

--- src/utils/file_process.py
+++ src/utils/file_process.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+from PIL import Image
+
 
 def c_r(dir_path, dst_path):
     dst_dir = os.path.join(dst_path, 'processed')
@@ -16,3 +18,15 @@ def c_r(dir_path, dst_path):
             dst_path = os.path.join(dst_dir, new_filename)
             shutil.copyfile(src_path, dst_path)
             i += 1
+    convert(dst_dir)
+
+
+def convert(dir_path):
+    for f in os.listdir(dir_path):
+        if not f.lower().endswith('.png'):
+            f_path = os.path.join(dir_path, f)
+            f_dir = dir_path
+            with Image.open(f_path) as img:
+                new_f = os.path.splitext(f)[0] + '.png'
+                img.save(os.path.join(f_dir, new_f))
+                os.remove(f_path)
--- src/utils/img_process.py
+++ src/utils/img_process.py
@@ -1,18 +1,6 @@
 import os
 import cv2
 import numpy as np
-from PIL import Image
-
-
-def convert(dir_path):
-    for f in os.listdir(os.path.join(dir_path, 'processed')):
-        if not f.lower().endswith('.png'):
-            f_path = os.path.join(dir_path, 'processed', f)
-            f_dir = os.path.join(dir_path, 'processed')
-            with Image.open(f_path) as img:
-                new_f = os.path.splitext(f)[0] + '.png'
-                img.save(os.path.join(f_dir, new_f))
-                os.remove(f_path)
 
 
 def crop(dir_path):
