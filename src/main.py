fea1b3793138bbe6c6ebd24ca131adcad7591234
lavaqq <mlava@tuta.io>
2023-03-23 22:06:06 +0100

    feat(main): args

--- src/main.py
+++ src/main.py
@@ -1,27 +1,37 @@
-from utils import file_process as fp
-from utils import img_process as ip
-
+from utils import f_prc
+from utils import img_prc
 import sys
 import argparse
 import os
 
+
 def main():
     parser = argparse.ArgumentParser(description='Process some images.')
-    parser.add_argument('image_dir', type=str, help='the directory containing the images to process')
-    parser.add_argument('-r', '--rename', action='store_true', help='rename the images')
+    parser.add_argument('src_path', type=str, help='the directory containing the images to process')
+    parser.add_argument('dst_path', type=str, help='the destination directory for the processed images')
     parser.add_argument('-b', '--blur', action='store_true', help='apply a blur effect to the images')
-    parser.add_argument('-c','--crop', action='store_true', help='crop the images to a square shape')
+    parser.add_argument('-c', '--crop', action='store_true', help='crop the images to a square shape')
     args = parser.parse_args()
-
-    image_dir = args.image_dir
-    rename = args.rename
+    src_p = args.src_path
+    dst_p = args.dst_path
     blur = args.blur
     crop = args.crop
-
-    if not os.path.isdir(image_dir):
-        print(f"Error: '{image_dir}' is not a directory.")
+    if not os.path.isdir(src_p):
+        print(f"Error: '{src_p}' is not a directory.")
         sys.exit(1)
-
-
+    if not blur and not crop:
+        print("Error: You must specify at least one operation to perform on the images.")
+        sys.exit(1)
+    f_prc.rc(src_p, dst_p)
+    if blur and crop:
+        img_prc.crop(dst_p)
+        img_prc.blur(dst_p)
+        sys.exit(1)
+    if blur:
+        img_prc.crop(dst_p)
+        sys.exit(1)
+    if crop:
+        img_prc.blur(dst_p)
+        sys.exit(1)
 if __name__ == '__main__':
     main()
