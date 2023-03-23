3396bdd7c0c8440292ebce92f3735bdf96bd061a
lavaqq <mlava@tuta.io>
2023-03-23 20:54:21 +0100

    fix(utils): NotADir

--- src/utils/f_prc.py
+++ src/utils/f_prc.py
@@ -3,30 +3,17 @@ import shutil
 from PIL import Image
 
 
-def ren_cp_conv(src_path, dst_path):
-    if os.path.exists(dst_path):
-        shutil.rmtree(dst_path)
-    os.mkdir(dst_path)
+def rc(src_p, dst_p):
+    if os.path.exists(dst_p):
+        shutil.rmtree(dst_p)
+    os.mkdir(dst_p)
     i = 0
-    for f in os.listdir(src_path):
-        f_path = os.path.join(src_path, f)
+    for f in os.listdir(src_p):
+        f_path = os.path.join(src_p, f)
         if os.path.isfile(f_path) and any(f_path.lower().endswith(ext) for ext in ['.jpg', '.jpeg', '.png', 'webp']):
             ext = os.path.splitext(f_path)[1]
-            new_filename = str(i) + ext
-            src_path = os.path.join(src_path, f)
-            dst_path = os.path.join(dst_path, new_filename)
-            shutil.copyfile(src_path, dst_path)
+            new_f = str(i) + ext
+            src_f = f_path
+            dst_f = os.path.join(dst_p, new_f)
+            shutil.copyfile(src_f, dst_f)
             i += 1
-    convert(dst_path)
-
-
-def convert(dst_path):
-    for f in os.listdir(dst_path):
-        if not f.lower().endswith('.png'):
-            f_path = os.path.join(dst_path, f)
-            f_dir = dst_path
-            with Image.open(f_path) as img:
-                new_f = os.path.splitext(f)[0] + '.png'
-                img.save(os.path.join(f_dir, new_f))
-                os.remove(f_path)
-                print(f"Info: '{f_path}' converted to PNG.")
