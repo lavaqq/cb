6efd9504fe14d4f13e74397d286c65bff5361db0
lavaqq <mlava@tuta.io>
2023-03-23 18:48:25 +0100

    refactor(utils): merge fn

--- src/utils/file_process.py
+++ src/utils/file_process.py
@@ -1,25 +1,18 @@
 import os
 import shutil
 
-def copy(dir_path):
+def c_r(dir_path):
     dst_dir = os.path.join(dir_path, 'processed')
     if os.path.exists(dst_dir):
         shutil.rmtree(dst_dir)
     os.mkdir(dst_dir)
+    i = 0
     for f in os.listdir(dir_path):
         f_path = os.path.join(dir_path, f)
         if os.path.isfile(f_path) and any(f_path.lower().endswith(ext) for ext in ['.jpg', '.jpeg', '.png']):
+            ext = os.path.splitext(f_path)[1]
+            new_filename = str(i) + ext
             src_path = os.path.join(dir_path, f)
-            dst_path = os.path.join(dst_dir, f)
+            dst_path = os.path.join(dst_dir, new_filename)
             shutil.copyfile(src_path, dst_path)
-
-
-def rename(dir_path):
-    i = 0
-    for f in os.listdir(os.path.join(dir_path, 'processed')):
-        ext = os.path.splitext(f)[1]
-        new_filename = str(i) + ext
-        src = os.path.join(dir_path, 'processed', f)
-        dst = os.path.join(dir_path, 'processed', new_filename)
-        os.rename(src, dst)
-        i += 1
+            i += 1
